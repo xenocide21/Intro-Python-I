a0aa20511b366279b9b552b4a009452efdbfeff5
Ashley Dunham <xenocide21@gmail.com>
2020-08-05 17:54:56 -0700

    hello printing lists tuples and dictionaries done

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,13 +35,30 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({'lat': 111, 'lon': -111, 'name': 'new waypoint'})
+
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
-
 # YOUR CODE HERE
+
+
+def not_real(waypoint):
+    for i in waypoint:
+        if i['name'] == 'a place':
+            i['lon'] = -130
+            i['name'] = 'not a real place'
+
+
+not_real(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+
+
+for i in waypoints:
+    print(i.keys())
+
+print(waypoints)
